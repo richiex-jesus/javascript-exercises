e23d34ad3c156081a1a35db1ba2937371c412a00
richiex-jesus <matiasrichard300@gmail.com>
2025-12-17 10:00:43 +0800

    completed reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let newString = str.split('');
+    newString.reverse();
+    let newStringFinal = newString.join('');
+    console.log(newStringFinal);
+    return newStringFinal;
+    
 };
 
+reverseString(str);
+
 // Do not edit below this line
-module.exports = reverseString;
+module.exports = reverseString; 
