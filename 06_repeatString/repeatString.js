814e8170c5e1b4a25cd847b8ce3890d4338c021d
richiex-jesus <matiasrichard300@gmail.com>
2025-12-17 09:22:56 +0800

    completed repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
-};
+    let result = "";
+
+    if (num < 0) {
+        return 'ERROR'
+    }
+
+    for (i = 1; i <= num; i++) {
+        result += string;
+    }
+
+    return result;
+}
+
+
+repeatString('hey', -1);
 
 // Do not edit below this line
 module.exports = repeatString;
