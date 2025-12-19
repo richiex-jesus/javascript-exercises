acc745f8bc9819ff2ae716e1bc08e96e2d439b63
richiex-jesus <matiasrichard300@gmail.com>
2025-12-19 13:28:47 +0800

    simplified algorithm for leapYears

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,8 +1,16 @@
 const leapYears = function(year) {
 
-    if (((year % 100 == 0) && (year % 400 !== 0)) || (year % 4 !== 0)) return false;
+    // if (((year % 100 == 0) && (year % 400 !== 0)) || (year % 4 !== 0)) return false;
 
-    if ((year % 4 == 0) || ((year % 400 == 0) && (year % 100 !== 0))) return true;
+    // if ((year % 4 == 0) || ((year % 400 == 0) && (year % 100 !== 0))) return true;
+
+    if (year % 4 !== 0) {
+        return false;
+    } else if ((year % 400 !== 0) && (year % 100 == 0)) {
+        return false;
+    } 
+
+    return true;
 
 };
 
