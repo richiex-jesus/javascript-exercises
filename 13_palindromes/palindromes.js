850534f84ae50313d3023c8081e568a985d2f02d
richiex-jesus <matiasrichard300@gmail.com>
2025-12-26 15:36:55 +0800

    completed palindromes assignment

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (someString) {
+    const cleanedString = someString.toLowerCase().replace(/[^a-z0-9]/g, '')
+    const reversedString = cleanedString.split("").reverse().join("");
+    return cleanedString === reversedString;
 };
+
+// SUPER IMPORTANT learning point: use the regex /[^a-z0-9]/g and replace it when I do not want any punctuation.
 
 // Do not edit below this line
 module.exports = palindromes;
