83ca4205190f3ebc2d97578f60f764914f8ad5ea
richiex-jesus <matiasrichard300@gmail.com>
2025-12-27 10:00:13 +0800

    attempted to findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const people = [
+    {   name: "Carly",
+        yearOfBirth: 1942,
+        yearOfDeath: 1970,
+    },
+    {   name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+    },
+    {   name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+    },
+]
 
+const findTheOldest = function(people) {
+    
+    // let newPeople = people.sort((a, b) => {
+    //     console.log(a.yearOfDeath - a.yearOfBirth);
+    //     console.log(b.yearOfDeath - b.yearOfBirth);
+    //     (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? -1 : 1;   
+    // })
+    // console.log(newPeople);
 };
 
 // Do not edit below this line
