5939660db85032b178f05f04282731beb8c2c347
richiex-jesus <matiasrichard300@gmail.com>
2025-12-27 09:31:23 +0800

    added curly braces to first fib func

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,31 +1,33 @@
 const fibonacci = function(num) {
-    // if (num < 0) return "ERROR!";
-    // if (num == 0) return '0';
-    // if (num > 0) {
-    //     const fibonacciSequence = [1, 1];
-    //     for (i = 2; i < num; i++) {
-    //         let newFibonacciNumber = fibonacciSequence[i - 1] + fibonacciSequence[i - 2];
-    //         fibonacciSequence[i] = newFibonacciNumber
-    //         // fibonacciSequence.splice(i - 1, 0, newFibonacciNumber); --> I realised I couldn't do this beacause it will add the number before the chosen index....
-    //     };
-    //     return fibonacciSequence;
-    // QUESTION: WHY DOES THIS RETURN UNDEFINED??
-
-    // VS: 
-
-    if (num < 0) {
-        return 'ERROR!';
-    } else if (num == 0) {
-        return 0;
-    } else {
+    if (num < 0) return "OOPS";
+    if (num == 0) return 0;
+    if (num > 0) {
         const fibonacciSequence = [1, 1];
         for (i = 2; i < num; i++) {
             let newFibonacciNumber = fibonacciSequence[i - 1] + fibonacciSequence[i - 2];
             fibonacciSequence[i] = newFibonacciNumber;
-        }
+            // fibonacciSequence.splice(i - 1, 0, newFibonacciNumber); --> I realised I couldn't do this beacause it will add the number before the chosen index....
+        };
         return fibonacciSequence[num - 1];
     }
 }
+    // QUESTION: WHY DOES THIS RETURN UNDEFINED??
+
+    // VS: 
+
+    // if (num < 0) {
+    //     return 'ERROR!';
+    // } else if (num == 0) {
+    //     return 0;
+    // } else {
+    //     const fibonacciSequence = [1, 1];
+    //     for (i = 2; i < num; i++) {
+    //         let newFibonacciNumber = fibonacciSequence[i - 1] + fibonacciSequence[i - 2];
+    //         fibonacciSequence[i] = newFibonacciNumber;
+    //     }
+    //     return fibonacciSequence[num - 1];
+    // }
+// }
     
     // VS: 
 
--- 14_fibonacci/fibonacci.spec.js
+++ 14_fibonacci/fibonacci.spec.js
@@ -1,10 +1,10 @@
 const fibonacci = require('./fibonacci')
 
 describe('fibonacci', () => {
-  test.skip('4th fibonacci number is 3', () => {
+  test('4th fibonacci number is 3', () => {
     expect(fibonacci(4)).toBe(3);
   });
-  test.skip('6th fibonacci number is 8', () => {
+  test('6th fibonacci number is 8', () => {
     expect(fibonacci(6)).toBe(8);
   });
   test('10th fibonacci number is 55', () => {
@@ -19,19 +19,19 @@ describe('fibonacci', () => {
   test('0th fibonacci number is 0', () => {
     expect(fibonacci(0)).toBe(0);
   });
-  test.skip('doesn\'t accept negatives', () => {
+  test('doesn\'t accept negatives', () => {
     expect(fibonacci(-25)).toBe("OOPS");
   });
-  test.skip('DOES accept strings', () => {
+  test('DOES accept strings', () => {
     expect(fibonacci("0")).toBe(0);
   });
-  test.skip('DOES accept strings', () => {
+  test('DOES accept strings', () => {
     expect(fibonacci("1")).toBe(1);
   });
-  test.skip('DOES accept strings', () => {
+  test('DOES accept strings', () => {
     expect(fibonacci("2")).toBe(1);
   });
-  test.skip('DOES accept strings', () => {
+  test('DOES accept strings', () => {
     expect(fibonacci("8")).toBe(21);
   });
 });
